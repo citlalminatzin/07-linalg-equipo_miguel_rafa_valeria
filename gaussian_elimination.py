6be896ebc0b6211cc96c63c2f92ebe4849522be8
rflsg <softsailors84@ciencias.unam.mx>
2026-04-10 16:56:05 -0600

    feat: ejercicio 1 y cambios a gauss

--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -19,12 +19,13 @@ def rowswap(M: list[list[float]], i: int, j: int):
 def gaussian_elimination(M: list[list[float]]):
     """Dada una matriz M, regresa una matriz triangular superior"""
     n = len(M)
+    U = [row for row in M]
 
     for i in range(n):
         # Encuentra el indice del pivote maximo
         pivotes = []
         for pivot in range(i, n):
-            pivotes.append(M[pivot][i])
+            pivotes.append(U[pivot][i])
         max_pivot = max(pivotes, key=abs)
         max_index = pivotes.index(max_pivot) + i
         if max_pivot == 0:
@@ -32,15 +33,15 @@ def gaussian_elimination(M: list[list[float]]):
             return None
 
         # Intercambia la fila con el pivote maximo con la actual
-        M = rowswap(M, i, max_index)
+        U = rowswap(U, i, max_index)
 
         # Elimina los valores debajo de la diagonal
         for j in range(i + 1, n):
-            aji = M[j][i]
-            pivot = M[i][i]
-            M[j] = rowsum(M, j, i, -aji / pivot)
+            aji = U[j][i]
+            pivot = U[i][i]
+            U[j] = rowsum(U, j, i, -aji / pivot)
 
-    return M
+    return U
 
 
 def triang_sup_to_diag(M: list[list[float]]):
@@ -49,19 +50,22 @@ def triang_sup_to_diag(M: list[list[float]]):
     regresa una matriz diagonal
     """
     n = len(M)
+    D = [row for row in M]
 
     for i in range(1, n):
         for j in range(i + 1, n + 1):
-            pivote = M[-i][-i]
-            aji = M[-j][-i]
-            M[-j] = rowsum(M, -j, -i, -aji / pivote)
+            pivote = D[-i][-i]
+            aji = D[-j][-i]
+            D[-j] = rowsum(D, -j, -i, -aji / pivote)
 
-    return M
+    return D
 
 
 def diag(M: list[list[float]]):
     """Dada una matriz regresa una matriz diagonalizada"""
-    gauss = gaussian_elimination(M)
+    D = [row for row in M]
+
+    gauss = gaussian_elimination(D)
     if gauss is not None:
         return triang_sup_to_diag(gauss)
     else:
@@ -69,17 +73,7 @@ def diag(M: list[list[float]]):
         return None
 
 
-def main():
-    A = [
-        [1.0, 2.0, -2.0, 1.0],
-        [4.0, 5.0, -7.0, 6.0],
-        [5.0, 25.0, -15.0, -3],
-        [6.0, -12.0, -6.0, 22.0],
-    ]
-    A = gaussian_elimination(A)
-    print(np.array(A))
-    A = triang_sup_to_diag(A)
-    print(np.array(A))
+def main(): ...
 
 
 if __name__ == "__main__":
