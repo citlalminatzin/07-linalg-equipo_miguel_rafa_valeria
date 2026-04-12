d6bc52833166839b64dd4c0a56650e73199e8723
Valeria Lazcano <valelf2005@gmail.com>
2026-04-11 19:58:08 -0600

    ejercicio 3

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 from gaussian_elimination import diag
+from qr import qr
+from gram_schmidt import matmul, matrix_to_str
 from lu import lu
 
 import numpy as np
@@ -32,7 +34,26 @@ def main():
             print(np.array(L), "\n")
             print("Producto LU")
             print(np.array(np.dot(L, U)), "\n")
-
+            
+    print("===Ejercicio3====")
+    for array in arrays:
+        with np.printoptions(precision=2):
+         print("Matriz inicial")
+         print(array, "\n")
+         A = array.tolist()
+         Q, R = qr(A)
+         print("Matriz Q (ortonormal)")
+         print(np.array(Q), "\n")
+         print("Matriz R (traingular superior)")
+         print(np.array(R), "\n")
+         QR = matmul(Q, R)
+         print("Producto QR")
+         print(np.array(QR), "\n")
+         print("¿QR ≈ A?")
+         if np.allclose(array, np.array(QR)):
+             print("Si coinciden \n")
+         else:
+             print("No coinciden \n")
 
 if __name__ == "__main__":
     main()
